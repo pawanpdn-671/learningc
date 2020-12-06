3047e6536c82ba2e31e202a4f41dad5f048ecfae
pawan247 <pawanpradhan247@gmail.com>
2020-12-06 21:59:41 +0530

    left to do

---
+++ output.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int ror(int arr[]);
+int main()
+{
+    int i,arr[100],number;
+    printf("Enter the number of the element of the Array : ");
+    scanf("%d",&number);
+    printf("Enter the elements of the Array : ");
+    for(i=0;i<=number;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("The odd number from the following are : ");
+    for(i=0;i<number;i++)
+    {
+        printf("%d",ror(arr));
+        
+    }
+    return 0;
+}
+int ror(int arr)
+{
+    
+}
