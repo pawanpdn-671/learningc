b3593c190fcd85138b2e3fe183fa5b77c0eb4e4f
pawan247 <pawanpradhan247@gmail.com>
2020-12-06 18:23:17 +0530

    finding area of circle

---
+++ radius.c
@@ -0,0 +1,10 @@
+#define PI 3.14
+int main()
+{
+    int r;float a;
+    printf("Enter redius of a circle");
+    scanf("%d",&r);
+    a=PI*r*r;
+    printf("Area of circle is %f",a);
+    return 0;
+}
