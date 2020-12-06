785c8296603edc25d455642286ae664f8639befe
pawan247 <pawanpradhan247@gmail.com>
2020-12-06 18:14:15 +0530

    break and continue keyword

---
+++ jumploop.c
@@ -0,0 +1,19 @@
+//Learning for loop
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        if(i==3)
+        {
+            continue;
+        }
+        if(i==5)
+        {
+            break;
+        }
+        printf("\n%d",i);
+    }
+    return 0;
+}
