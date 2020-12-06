c1bbb7beeb4c86a6df8e617cd3967a8a02c9aa74
pawan247 <pawanpradhan247@gmail.com>
2020-12-06 18:15:24 +0530

    numbers pattern from user

---
+++ loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,s,j;
+    scanf("%d",&s);
+    for(i=1;i<=s;i++)      
+    {
+        for(j=1;j<=s;j++)
+       {
+            printf("%d",i);
+       }
+       printf("\n");
+    }
+return 0;
+}
