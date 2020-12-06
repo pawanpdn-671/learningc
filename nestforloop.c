d3b8781d89fbe9476db0a910099198cc74989714
pawan247 <pawanpradhan247@gmail.com>
2020-12-06 18:17:46 +0530

    simple star pattern

---
+++ nestforloop.c
@@ -0,0 +1,15 @@
+//Nesting of Loops
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
