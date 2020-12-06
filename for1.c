3165f7c734399eb8260ac9a859b71893d05fb545
pawan247 <pawanpradhan247@gmail.com>
2020-12-06 18:03:16 +0530

    for loop of two variables

---
+++ for1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,k;
+    for (i=1,k=2; k<=20,i<=5; i++,k=k+2)
+    {
+        printf("\n\tI=%d",i);
+        printf("\tK=%d",k);
+    
+    }
+return 0;
+}
